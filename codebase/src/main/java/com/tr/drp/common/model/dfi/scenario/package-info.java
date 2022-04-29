0608fe96e4485e991f508c6c38ac4e0e3705df8c
YuriyGronaTR <yuriy.grona@thomsonreuters.com>
2022-04-29 08:13:25 +0300

    fixes xml

---
+++ codebase/src/main/java/com/tr/drp/common/model/dfi/scenario/package-info.java
@@ -0,0 +1,7 @@
+@XmlSchema(
+        namespace="http://www.thomsonreuters.com/ositax/integrations/datafileintegrator/model/v1_1",
+        elementFormDefault= XmlNsForm.QUALIFIED)
+package com.tr.drp.common.model.dfi.scenario;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
